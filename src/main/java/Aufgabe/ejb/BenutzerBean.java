aeaf4b4b3f4857f88fb36c884fb36ce672f0a626
Samuel Haag <Home@DESKTOP-DUAEADE>
2018-02-27 13:17:05 +0100

    Methoden in den EJB
    CRUD-Methoden in den EJBs hinzugefügt

--- src/main/java/Aufgabe/ejb/BenutzerBean.java
+++ src/main/java/Aufgabe/ejb/BenutzerBean.java
@@ -5,6 +5,8 @@
  */
 package Aufgabe.ejb;
 
+import Aufgabe.jpa.Benutzer;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -15,6 +17,60 @@ import javax.persistence.PersistenceContext;
  */
 @Stateless
 public class BenutzerBean {
+
     @PersistenceContext
     EntityManager em;
+
+    /**
+     * Anlegen eines neuen Benutzers.
+     *
+     *
+     * @return Der angelegte Benutzer
+     */
+    public Benutzer createNewBenutzer(String benutzername, String passwort, String vorname, String nachname, String strasse, int hausnummer, int plz, String ort, String land, String email, String telefonnummer) {
+        Benutzer benutzer = new Benutzer(benutzername, passwort, vorname, nachname, strasse, hausnummer, plz, ort, land, email, telefonnummer);
+        em.persist(benutzer);
+        return em.merge(benutzer);
+    }
+
+    /**
+     * @return Liste mit allen Benutzern in umgedrehter Anlagereihenfolge
+     */
+    public List<Benutzer> findAllBenutzer() {
+        return em.createQuery("SELECT w FROM Benutzer w ORDER BY w.benutzername DESC").getResultList();
+    }
+
+    /**
+     * Einzelnen Benutzer ermitteln
+     *
+     * @param benutzername ID des Benutzer
+     * @return Gefundenes Objekt oder null
+     */
+    public Benutzer findBenutzer(String benutzername) {
+        return em.find(Benutzer.class, benutzername);
+    }
+
+    /**
+     * Löschen eines Benutzers
+     * @param benutzername ID des zu löschenden Benutzers
+     * @return Der gelöschte Benutzer oder null
+     */
+    public Benutzer deleteBenutzer(String benutzername) {
+        Benutzer benutzer = em.find(Benutzer.class, benutzername);
+
+        if (benutzer != null) {
+            em.remove(benutzer);
+        }
+
+        return benutzer;
+    }
+
+    /**
+     * Änderungen an einem Benutzer speichern.
+     * @param benutzer Der zu ändernde Textschnippsel
+     * @return Neue, gespeicherte Version des Benutzers
+     */
+    public Benutzer updateBenutzer(Benutzer benutzer) {
+        return em.merge(benutzer);
+    }
 }
