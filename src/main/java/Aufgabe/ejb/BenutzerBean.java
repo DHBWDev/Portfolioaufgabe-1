82c509fd6dfadee3ead0d63dc32360bc48bf2283
Samuel Haag <>
2018-02-27 12:10:02 +0100

    Methoden in Anzeige und IndexServlet
    Getter- und Setter-Methoden, Konstruktor in Anzeige hinzugefügt
    doPost und doGet-Methoden in IndexServlet hinzugefügt

--- src/main/java/Aufgabe/ejb/BenutzerBean.java
+++ src/main/java/Aufgabe/ejb/BenutzerBean.java
@@ -18,4 +18,3 @@ public class BenutzerBean {
     @PersistenceContext
     EntityManager em;
 }
-    
--- src/main/java/Aufgabe/web/IndexServlet.java
+++ src/main/java/Aufgabe/web/IndexServlet.java
@@ -5,8 +5,12 @@
  */
 package Aufgabe.web;
 
+import java.io.IOException;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  *
@@ -14,5 +18,15 @@ import javax.servlet.http.HttpServlet;
  */
 @WebServlet(urlPatterns="/index.html")
 public class IndexServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
